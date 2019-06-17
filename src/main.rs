f092065d2380fc9425d8d7507a8a9b947f8143c9
Михаил Красильников <m.krasilnikov@webbankir.com>
2019-06-17 12:09:41 +0300

    feat: возможность задавать верхний порог

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,99 @@
 #[macro_use]
-
 extern crate clap;
 extern crate exitcode;
 extern crate regex;
 
 use clap::App;
+use clap::Values;
 use regex::Regex;
 use std::process::Command;
 use std::process::exit;
 use std::string::String;
+use ::ThresholdKind::{Lower, Higher};
 
+/// Виды пороговых значений.
+#[derive(Copy, Clone)]
+enum ThresholdKind {
+    Lower,
+    Higher,
+}
+
+
+/// Пороговое значение.
+struct Threshold {
+    /// Имя параметра.
+    parameter: String,
+    /// Вид порога.
+    kind: ThresholdKind,
+    /// Пороговое значение.
+    value: f32,
+}
+
+impl Threshold {
+    fn satisfied(&self, value: f32) -> bool {
+        match self.kind {
+            Lower => value >= self.value,
+            Higher => value <= self.value,
+        }
+    }
+}
+
+/// Производит разбор пороговых аргументов.
+///
+/// - kind — вид пороговых значений
+/// - values — значения аргументов для этого вида порогов из командной строки
+fn parse_threshold_args(kind: ThresholdKind, values: Values) -> Vec<Threshold> {
+    let mut thresholds: Vec<Threshold> = vec![];
+
+    for value in values {
+        let parts: Vec<&str> = value.split("=").collect();
+        thresholds.push(
+            Threshold {
+                parameter: parts[0].to_string(),
+                kind,
+                value: match parts[1].parse::<f32>() {
+                    Ok(value) => value,
+                    Err(error) => {
+                        println!(
+                            "{} value should be a float: {}.",
+                            parts[0].to_string(),
+                            error
+                        );
+                        exit(exitcode::DATAERR);
+                    }
+                },
+            }
+        );
+    }
+
+    return thresholds;
+}
+
+/// Главная функция.
 fn main() {
+
+    // Список пороговых значений.
+    let mut thresholds: Vec<Threshold> = vec![];
+
+    // Разбираем аргументы вызова.
+
     let yaml = load_yaml!("cli.yml");
     let arguments = App::from_yaml(yaml).get_matches();
 
     let mut command = arguments.values_of("COMMAND").unwrap();
     let pattern = arguments.value_of("pattern").unwrap();
-    let mut lowers = arguments.values_of("lowers").unwrap();
-    let mut highers = arguments.values_of("highers").unwrap();
+
+    if arguments.is_present("lower") {
+        for argument in arguments.values_of("lower") {
+            thresholds.append(&mut parse_threshold_args(Lower, argument));
+        }
+    }
+
+    if arguments.is_present("higher") {
+        for argument in arguments.values_of("higher") {
+            thresholds.append(&mut parse_threshold_args(Higher, argument));
+        }
+    }
 
     let process = match Command::new(command.next().unwrap())
         .args(command)
@@ -28,31 +104,53 @@ fn main() {
 
     let output = String::from_utf8(process.stdout).unwrap();
 
-    let mut exit_code = exitcode::OK;
+    // Список найденных проблем.
+    let mut failures: Vec<String> = vec![];
 
     let re = Regex::new(pattern).unwrap();
     for matches in re.captures_iter(output.as_str()) {
-        let lower_thresholds = &mut lowers;
-        for threshold in lower_thresholds {
-            let parts: Vec<&str> = threshold.split("=").collect();
-            let actual_value = matches.name(parts[0]).unwrap().as_str();
-            let threshold_value = parts[1];
-            if actual_value < threshold_value {
-                exit_code = exitcode::DATAERR;
+        for threshold in &thresholds {
+            let matched = matches.name(threshold.parameter.as_str());
+            if matched == None {
+                continue;
             }
-        }
-        let higher_thresholds = &mut highers;
-        for threshold in higher_thresholds {
-            let parts: Vec<&str> = threshold.split("=").collect();
-            let actual_value = matches.name(parts[0]).unwrap().as_str();
-            let threshold_value = parts[1];
-            if actual_value > threshold_value {
-                exit_code = exitcode::DATAERR;
+
+            let value = match matched.unwrap().as_str().parse::<f32>() {
+                Ok(value) => value,
+                Err(error) => {
+                    println!(
+                        "Matched value for {} should be a float: {}.",
+                        threshold.parameter.as_str(),
+                        error
+                    );
+                    exit(exitcode::DATAERR);
+                }
+            };
+
+            if !threshold.satisfied(value) {
+                failures.push(
+                    format!(
+                        "{} value {} is {} than {}",
+                        threshold.parameter,
+                        value,
+                        match threshold.kind {
+                            Lower => "lower",
+                            Higher => "higher",
+                        },
+                        threshold.value
+                    )
+                );
             }
         }
     }
 
     println!("{}", output.as_str());
 
-    exit(exit_code);
+    if failures.len() > 0 {
+        println!("Threshold failures:");
+        for failure in failures {
+            println!(" ⋅ {}", failure);
+        }
+        exit(exitcode::DATAERR);
+    }
 }
