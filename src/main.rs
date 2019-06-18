1898416f076870ffbd60fce72aafdd8ccb564455
Михаил Красильников <m.krasilnikov@yandex.ru>
2019-06-18 23:39:28 +0300

    refactor: извлечение значений отделено от их проверки

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ extern crate exitcode;
 extern crate regex;
 
 use clap::App;
-use clap::Values;
+use clap::Values as InputValues;
 use regex::Regex;
+use std::collections::HashMap;
 use std::process::Command;
 use std::process::exit;
 use std::string::String;
-use ::ThresholdKind::{Lower, Higher};
 
 /// Виды пороговых значений.
 #[derive(Copy, Clone)]
@@ -17,7 +17,6 @@ enum ThresholdKind {
     Lower,
     Higher,
 }
-
 
 /// Пороговое значение.
 struct Threshold {
@@ -32,8 +31,8 @@ struct Threshold {
 impl Threshold {
     fn satisfied(&self, value: f32) -> bool {
         match self.kind {
-            Lower => value >= self.value,
-            Higher => value <= self.value,
+            ThresholdKind::Lower => value >= self.value,
+            ThresholdKind::Higher => value <= self.value,
         }
     }
 }
@@ -44,7 +43,7 @@ impl Threshold {
 /// - kind — вид пороговых значений
 /// - values — значения аргументов для этого вида порогов из командной строки
 ///
-fn parse_threshold_args(kind: ThresholdKind, values: Values) -> Vec<Threshold> {
+fn parse_threshold_args(kind: ThresholdKind, values: InputValues) -> Vec<Threshold> {
     let mut thresholds: Vec<Threshold> = vec![];
 
     for value in values {
@@ -72,47 +71,78 @@ fn parse_threshold_args(kind: ThresholdKind, values: Values) -> Vec<Threshold> {
 }
 
 ///
-/// Обрабатывает вывод выполненной команды.
+/// Извлекает значения из вывода выполненной команды.
 ///
-/// Возвращает список найденных проблем.
+/// - output — вывод команды
+/// - pattern — шаблон для поиска значений
 ///
-fn process_output(output: &String, pattern: Regex, thresholds: Vec<Threshold>) -> Vec<String> {
-    // Список найденных проблем.
-    let mut failures: Vec<String> = vec![];
+fn extract_values(output: &String, pattern: Regex) -> HashMap<String, f32> {
+    // Список найденных значений.
+    let mut values = HashMap::new();
 
     for matches in pattern.captures_iter(output.as_str()) {
-        for threshold in &thresholds {
-            let matched = matches.name(threshold.parameter.as_str());
-            if matched == None {
-                continue;
-            }
+        for capture_name in pattern.capture_names() {
+            let name = match capture_name {
+                None => continue,
+                Some(name) => name,
+            };
 
-            let value = match matched.unwrap().as_str().parse::<f32>() {
+            let one_match = match matches.name(name) {
+                None => continue,
+                Some(m) => m,
+            };
+
+            let value = match one_match.as_str().parse::<f32>() {
                 Ok(value) => value,
                 Err(error) => {
                     println!(
-                        "Matched value for {} should be a float: {}.",
-                        threshold.parameter.as_str(),
+                        "Matched value for \"{}\" should be a float: {}.",
+                        name,
                         error
                     );
                     exit(exitcode::DATAERR);
                 }
             };
 
-            if !threshold.satisfied(value) {
-                failures.push(
-                    format!(
-                        "{} value {} is {} than {}",
-                        threshold.parameter,
-                        value,
-                        match threshold.kind {
-                            Lower => "lower",
-                            Higher => "higher",
-                        },
-                        threshold.value
-                    )
-                );
+            let entry = values.entry(name.to_string()).or_insert(value);
+            *entry = value;
+        }
+    }
+
+    return values;
+}
+
+///
+/// Проверяет значения на соответствие границам.
+///
+/// Возвращает список найденных проблем.
+///
+fn check_thresholds(values: HashMap<String, f32>, thresholds: Vec<Threshold>) -> Vec<String> {
+    // Список найденных проблем.
+    let mut failures: Vec<String> = vec![];
+
+    for threshold in &thresholds {
+        let value = match values.get(threshold.parameter.as_str()) {
+            None => {
+                println!("Value fo \"{}\" not found in command output.", threshold.parameter);
+                exit(exitcode::DATAERR);
             }
+            Some(value) => *value
+        };
+
+        if !threshold.satisfied(value) {
+            failures.push(
+                format!(
+                    "{} value {} is {} than {}",
+                    threshold.parameter,
+                    value,
+                    match threshold.kind {
+                        ThresholdKind::Lower => "lower",
+                        ThresholdKind::Higher => "higher",
+                    },
+                    threshold.value
+                )
+            );
         }
     }
 
@@ -135,13 +165,13 @@ fn main() {
 
     if arguments.is_present("lower") {
         for argument in arguments.values_of("lower") {
-            thresholds.append(&mut parse_threshold_args(Lower, argument));
+            thresholds.append(&mut parse_threshold_args(ThresholdKind::Lower, argument));
         }
     }
 
     if arguments.is_present("higher") {
         for argument in arguments.values_of("higher") {
-            thresholds.append(&mut parse_threshold_args(Higher, argument));
+            thresholds.append(&mut parse_threshold_args(ThresholdKind::Higher, argument));
         }
     }
 
@@ -153,10 +183,10 @@ fn main() {
     };
 
     let output = String::from_utf8(process.stdout).unwrap();
-
-    let failures: Vec<String> = process_output(&output, regex, thresholds);
-
     println!("{}", output.as_str());
+
+    let values = extract_values(&output, regex);
+    let failures = check_thresholds(values, thresholds);
 
     if failures.len() > 0 {
         println!("Threshold failures:");
